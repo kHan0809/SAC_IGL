3c88530add2b92361849e33c5afb1ae0c575ab77
kHan0809 <kimhangyeol14@gmail.com>
2023-04-19 18:17:53 +0900

    network edit -1
    network edit -1

--- model/SAC_HER.py
+++ model/SAC_HER.py
@@ -150,7 +150,7 @@ class SAC_HER(object):
 def mlp(sizes, activation, output_activation=nn.Identity()):
     layers = []
     for j in range(len(sizes)-1):
-        act = activation if j < len(sizes)-2 else output_activation
+        act = activation if j < len(sizes)-1 else output_activation
         layers += [nn.Linear(sizes[j], sizes[j+1]), act]
     return nn.Sequential(*layers)
 
--- model/SAC_HER_OVER.py
+++ model/SAC_HER_OVER.py
@@ -192,7 +192,7 @@ class SAC_HER_OVER(object):
 def mlp(sizes, activation, output_activation=nn.Identity()):
     layers = []
     for j in range(len(sizes)-1):
-        act = activation if j < len(sizes)-2 else output_activation
+        act = activation if j < len(sizes)-1 else output_activation
         layers += [nn.Linear(sizes[j], sizes[j+1]), act]
     return nn.Sequential(*layers)
 
