87e4b5c50f12e91924d40a476a131b05e11b119b
kHan0809 <kimhangyeol14@gmail.com>
2023-04-19 18:24:07 +0900

    .
    .

--- run_experiments.sh
+++ run_experiments.sh
@@ -5,14 +5,24 @@
 envs=(
 #    "FetchReach-v1"
     "FetchPush-v1"
-#    "FetchPickAndPlace-v1"
+    "FetchPickAndPlace-v1"
 	)
 
 for ((i=0;i<5;i+=1))
 do 
 	for env in ${envs[*]}
 	do
-		python Run_SAC+HER+OVER.py \
+		python Run_SAC+HER+IGL.py \
+		--env $env \
+		--seed $i
+	done
+done
+
+for ((i=0;i<5;i+=1))
+do
+	for env in ${envs[*]}
+	do
+		python Run_SAC+HER.py \
 		--env $env \
 		--seed $i
 	done
